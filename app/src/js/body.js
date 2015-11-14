267ae941919a04710836299a71bd3870f1f0ebaa
Kenneth <syltaxue@hotmail.com>
2015-11-14 10:28:14 -0800

    Finish the style of body

--- app/src/js/body.js
+++ app/src/js/body.js
@@ -4,20 +4,28 @@ var ProductItem = require("./components/productItem");
 var Body = React.createClass({
 	render: function() {
 		return (
-			<div className = "body">
-				<div className = "body-container">
-					<div className = "body-recommendText">推荐/精选</div>
-					<div className = "body-products">
-						<ProductItem />
-						<ProductItem />
-						<ProductItem />
-						<ProductItem />
-						<ProductItem />
-						<ProductItem />
-						<ProductItem />
-						<ProductItem />
-						<ProductItem />
+			<div className = "row">
+				<div className = "col-sm-1">
+				</div>
+				<div className = "col-xs-12 col-sm-10">
+					<div className = "body">
+						<div className = "body-container">
+							<div className = "body-recommendText">推荐/精选</div>
+							<div className = "body-products">
+								<ProductItem />
+								<ProductItem />
+								<ProductItem />
+								<ProductItem />
+								<ProductItem />
+								<ProductItem />
+								<ProductItem />
+								<ProductItem />
+								<ProductItem />
+							</div>
+						</div>
 					</div>
+				</div>
+				<div className = "col-sm-1">
 				</div>
 			</div>
 		);
