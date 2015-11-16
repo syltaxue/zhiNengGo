b9e9d20f06043a90a692dc8910e1e95004849bbc
Kenneth <syltaxue@hotmail.com>
2015-11-15 17:37:52 -0800

    Layout for header

--- app/src/js/header.js
+++ app/src/js/header.js
@@ -6,15 +6,24 @@ var Header = React.createClass({
 		return (
 			<div className = "header">
 				<div className = "header-container">
-					<div className = "header-siteName">
-						智能购
-					</div>
-					<div className = "header-searchEngine">
-						搜索
-					</div>
-					<div className = "header-login">
-						<RaisedButton label="登陆/注册" onClick={this._onClickLogin} primary={true} />
-						
+					<div className= "row">
+						<div className = "col-sm-1"></div>
+						<div className = "col-xs-5 col-sm-4">
+							<div className = "header-siteName">
+								智能GO
+							</div>
+						</div>
+						<div className = "col-xs-5 col-sm-4">
+							<div className = "header-searchEngine">
+								搜索
+							</div>
+						</div>
+						<div className = "col-xs-2 col-sm-2">
+							<div className = "header-login">
+								<RaisedButton label="登陆/注册" onClick={this._onClickLogin} primary={true} />
+							</div>
+						</div>
+						<div className= "col-sm-1"></div>
 					</div>
 				</div>
 			</div>
