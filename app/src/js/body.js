8c8ed4580c685e63773d8f97ddc96085f9b93e62
Kenneth <syltaxue@hotmail.com>
2015-11-18 23:39:25 -0800

    WIP Jump to top button

--- app/src/js/body.js
+++ app/src/js/body.js
@@ -6,28 +6,61 @@ var IconButton = require('material-ui/lib/icon-button');
 var Body = React.createClass({
 	render: function() {
 		return (
-			<div className = "row">
-				<div className = "col-sm-1">
-				</div>
-				<div className = "col-xs-12 col-sm-10">
-					<div className = "body">
-						<div className = "body-container">
-							<div className = "body-recommendText">推荐/精选</div>
-							<div className = "body-products">
-								<ProductItem />
-								<ProductItem />
-								<ProductItem />
-								<ProductItem />
-								<ProductItem />
-								<ProductItem />
-								<ProductItem />
-								<ProductItem />
-								<ProductItem />
+			<div id ="bodyContext">
+				<div className = "row">
+					<div className = "col-sm-1">
+					</div>
+					<div className = "col-xs-12 col-sm-9">
+						<div className="ui right rail">
+					<div className = "ui bound bottom sticky">
+						top
+					</div>
+						</div>
+						<div className = "body">
+							<div className = "body-container">
+								<div className = "body-recommendText">推荐/精选</div>
+								<div className = "body-products">
+									<ProductItem />
+									<ProductItem />
+									<ProductItem />
+									<ProductItem />
+									<ProductItem />
+									<ProductItem />
+									<ProductItem />
+									<ProductItem />
+									<ProductItem />
+									<ProductItem />
+									<ProductItem />
+									<ProductItem />
+									<ProductItem />
+									<ProductItem />
+									<ProductItem />
+									<ProductItem />
+									<ProductItem />
+									<ProductItem />
+									<ProductItem />
+									<ProductItem />
+									<ProductItem />
+									<ProductItem />
+									<ProductItem />
+									<ProductItem />
+									<ProductItem />
+									<ProductItem />
+									<ProductItem />
+									<ProductItem />
+									<ProductItem />
+									<ProductItem />
+									<ProductItem />
+									<ProductItem />
+									<ProductItem />
+									<ProductItem />
+									<ProductItem />
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-				<div className = "col-sm-1">
+					<div className = "col-sm-2">
+					</div>
 				</div>
 			</div>
 		);
