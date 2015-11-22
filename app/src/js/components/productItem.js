177150f27062e408ebd2c5c7f0263313c3347105
Kenneth <syltaxue@hotmail.com>
2015-11-21 16:28:39 -0800

    Format the spaces

--- app/src/js/components/productItem.js
+++ app/src/js/components/productItem.js
@@ -5,16 +5,13 @@ var ProductItem = React.createClass({
 	render: function() {
 		return (
 			<div className = "productItem">单个商品
-			<div className="ui labeled button" tabindex="0">
-  								<div className="ui red button">
-    							<i className="heart icon"></i> Like
-  </div>
-  <a className="ui basic red left pointing label">
-    1,048
-  </a>
-</div>	
+			<div className="ui labeled button" tabIndex="0">
+				<div className="ui red button">
+					<i className="heart icon"></i> Like
+				</div>
+				<a className="ui basic red left pointing label">1,048</a>
+			</div>	
 			</div>
-
 		);
 	}
 });
--- app/src/js/navigator.js
+++ app/src/js/navigator.js
@@ -1,11 +1,11 @@
 var React = require("react");
 var DropDownMenu = require('material-ui/lib/drop-down-menu');
 var menuItems = [
-   { payload: '1', text: '分类' },
-   { payload: '2', text: '类型1' },
-   { payload: '3', text: '类型2' },
-   { payload: '4', text: '类型3' },
-   { payload: '5', text: '类型4' },
+	{ payload: '1', text: '分类' },
+	{ payload: '2', text: '类型1' },
+	{ payload: '3', text: '类型2' },
+	{ payload: '4', text: '类型3' },
+	{ payload: '5', text: '类型4' }
 ];
 var Navigator = React.createClass({
 	render: function() {
@@ -14,8 +14,7 @@ var Navigator = React.createClass({
 				<div className = "navigator-container">
 					<div className = "navigator-buttons">
 						<div className = "navigator-buttons-button-firstChild">首页</div>
-            	   
-      				<div className = "navigator-buttons-button"><DropDownMenu menuItems={menuItems} /></div>
+						<div className = "navigator-buttons-button"><DropDownMenu menuItems={menuItems} /></div>
 						<div className = "navigator-buttons-button"><DropDownMenu menuItems={menuItems} /></div>
 						<div className = "navigator-buttons-button"><DropDownMenu menuItems={menuItems} /></div>
 						<div className = "navigator-buttons-button"><DropDownMenu menuItems={menuItems} /></div>
