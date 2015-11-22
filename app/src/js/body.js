ec88ee4d3ba4e80cf0421b0034ee8fd00fc5b036
Kenneth <syltaxue@hotmail.com>
2015-11-22 00:01:23 -0800

    Load more button complete

--- app/src/js/body.js
+++ app/src/js/body.js
@@ -4,13 +4,19 @@ var IconButton = require('material-ui/lib/icon-button');
 
 
 var Body = React.createClass({
+	getInitialState: function() {
+		return {
+			loadingMoreItem: false,
+			numberOfProducts: 10
+		};
+	},
 	render: function() {
 		return (
 			<div id ="bodyContext">
 				<div className = "row">
 					<div className = "col-sm-1">
 					</div>
-					<div className = "col-xs-12 col-sm-9">
+					<div className = "col-xs-12 col-sm-10">
 						<div className="ui right rail">
 							<div className = "ui bound bottom sticky">
 								<button className="ui icon button" onClick={this.scrollToTop}>
@@ -21,47 +27,14 @@ var Body = React.createClass({
 						<div className = "body">
 							<div className = "body-container">
 								<div className = "body-recommendText">推荐/精选</div>
-								<div className = "body-products">
-									<ProductItem />
-									<ProductItem />
-									<ProductItem />
-									<ProductItem />
-									<ProductItem />
-									<ProductItem />
-									<ProductItem />
-									<ProductItem />
-									<ProductItem />
-									<ProductItem />
-									<ProductItem />
-									<ProductItem />
-									<ProductItem />
-									<ProductItem />
-									<ProductItem />
-									<ProductItem />
-									<ProductItem />
-									<ProductItem />
-									<ProductItem />
-									<ProductItem />
-									<ProductItem />
-									<ProductItem />
-									<ProductItem />
-									<ProductItem />
-									<ProductItem />
-									<ProductItem />
-									<ProductItem />
-									<ProductItem />
-									<ProductItem />
-									<ProductItem />
-									<ProductItem />
-									<ProductItem />
-									<ProductItem />
-									<ProductItem />
-									<ProductItem />
+								{this.renderProducts()}
+								<div className = "body-loadMoreBtn">
+									{this.renderLoadMoreBtn()}
 								</div>
 							</div>
 						</div>
 					</div>
-					<div className = "col-sm-2">
+					<div className = "col-sm-1">
 					</div>
 				</div>
 			</div>
@@ -70,6 +43,49 @@ var Body = React.createClass({
 
 	scrollToTop: function() {
 		window.scrollTo(0, 0);
+	},
+
+	renderLoadMoreBtn: function() {
+		if (this.state.loadingMoreItem) {
+			return (
+				<button className="ui loading button" id="loadMoreBtn">Loading</button>
+			);
+		} else {
+			return (
+				<button className="ui button" id="loadMoreBtn" onClick={this.loadMoreProducts}>点击加载更多</button>
+			);
+		}
+	},
+
+	renderProducts: function() {
+		var arrayContainer = Array.apply(null, Array(this.state.numberOfProducts)).map(function () {});
+		var products = arrayContainer.map(function(item, index) {
+			return (
+				<ProductItem key={index}/>
+			);
+		});
+
+		var renderComponents = (
+			<div className = "body-products">
+				{products}
+			</div>
+		);
+
+		return products;
+	},
+
+	loadMoreProducts: function() {
+		this.setState({
+			loadingMoreItem: true,
+			numberOfProducts: this.state.numberOfProducts + 10
+		});
+
+
+	setTimeout(function(){
+		this.setState({
+			loadingMoreItem: false
+		});
+	}.bind(this), 1000);
 	}
 });
 
