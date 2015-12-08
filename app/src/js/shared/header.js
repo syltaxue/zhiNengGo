79ac252acf423289c3d332df0eb990e84fe9de7c
Kai Guo <guokyle19920714@gmail.com>
2015-12-07 18:30:06 -0800

    智能GO logo 点击返回主页

--- app/src/js/shared/header.js
+++ app/src/js/shared/header.js
@@ -3,15 +3,24 @@ var RaisedButton = require('material-ui/lib/raised-button');
 var Link = require('react-router').Link;
 
 var Header = React.createClass({
+	getInitialState: function() {
+		return ({active: 'home'});
+	},
+	shouldComponentUpdate: function(nextProps, nextState) {
+		if (this.props !== nextProps) return true;
+		if (this.state && this.state.active !== nextState.active) return true;
+		return false;
+	},
 	render: function() {
+		var homeButtonState = this.state.active === 'home' ? ' active' : '';
 		return (
 			<div className = "header">
 				<div className = "header-container">
 					<div className= "row">
 						<div className = "col-sm-1"></div>
 						<div className = "col-xs-4 col-sm-3">
-							<div className = "header-siteName">
-								智能GO
+							<div className = "item" onClick={this._handleClick.bind(this, 'home')}>
+								<Link className="header-buttons-text" to={'/'}>智能GO</Link>
 							</div>
 						</div>
 						<div className = "col-xs-6 col-sm-5">
@@ -45,6 +54,9 @@ var Header = React.createClass({
 
 	_onClickSearch: function() {
 		window.alert("Search clicked");
+	},
+	_handleClick: function(type) {
+		this.setState({active: type});
 	}
 });
 
