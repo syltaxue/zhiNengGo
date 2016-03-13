56be8716e7cdd1af657c5dc02f50e84688a00b86
Kenneth <syltaxue@hotmail.com>
2016-03-12 21:00:53 -0800

    database setup

--- app/src/server/server.js
+++ app/src/server/server.js
@@ -1,14 +1,41 @@
 var path = require('path');
 var fs = require('fs');
 var express = require('express');
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+	host : 'localhost',
+	user : 'root',
+	password : '19930208',
+	database : 'cs304'
+});
 
 // Create an express instance and set a port variable
 
 var app = express();
-app.use('/', express.static(path.join(__dirname, 'public')));
+// app.use('/', express.static(path.join(__dirname, 'public')));
+
+connection.connect(function(err){
+	if (!err) {
+		console.log("Database is connected ...");    
+	} else {
+		console.log("Error connecting database :", err);    
+	}
+});
+
+
+
+
 
 var server = app.listen(80);
 console.log('Server listening on port 80');
+
+connection.query('SELECT * from recipes', function(err, rows, fields) {
+connection.end();
+if (!err)
+	console.log('The query result from database is: ', rows);
+else
+	console.log('Error while performing Query.');
+});
 
 // Socket.IO part
 var io = require('socket.io')(server);
