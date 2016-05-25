92b75b0f90ae793e274b2039830bd4b804c2e4fb
syltaxue <syltaxue@hotmail.com>
2016-05-25 02:29:54 -0700

    update product store

--- app/src/js/stores/productStore.js
+++ app/src/js/stores/productStore.js
@@ -3,12 +3,6 @@ var productActions = require("./../actions/productActions");
 
 var products = {};
 
-function getItemByKey(list,itemKey){
-    return _.find(list, function(item) {
-        return item.key === itemKey;
-    });
-}
-
 var productStore = Reflux.createStore({
     // this will set up listeners to all publishers in TodoActions, using onKeyname (or keyname) as callbacks
     listenables: [productActions],
@@ -18,30 +12,24 @@ var productStore = Reflux.createStore({
             description: product.description
         };
     },
+    onFetchProducts: function() {
+        // Get all products here
+        products[product.id] = {
+            id: product.id,
+            description: product.description
+        };
+        updateList(this.products);
+    },
     // called whenever we change a list. normally this would mean a database API call
     updateList: function(products){
-        localStorage.setItem(localStorageKey, JSON.stringify(list));
-        // if we used a real database, we would likely do the below in a callback
-        this.products = product;
-        this.trigger(list); // sends the updated list to all listening components (TodoApp)
+        this.trigger({products: this.products}); // sends the updated list to all listening components (TodoApp)
     },
     // this will be called by all listening components as they register their listeners
     getInitialState: function() {
-        var loadedList = false;
-        if (!loadedList) {
-            // If no list is in localstorage, start out with a default one
-            this.products = [{
-                id: 1,
-                description: "something"
-            }];
-        } else {
-            this.products = _.map(JSON.parse(loadedList), function(item) {
-                // just resetting the key property for each todo item
-                item.key = todoCounter++;
-                return item;
-            });
-        }
-        return this.products;
+        this.products = [{
+            id: 1,
+            description: "something"
+        }];
     }
 });
 
