716bd0f7e7bd8470f85f83be5357fb6a13502420
syltaxue <syltaxue@hotmail.com>
2016-05-30 12:51:11 -0700

    Updated navigator, having the navigator fixed at top when the page is scrolled down. Updated the picture part below the navigator, replacing with a carousel model

--- app/src/js/shared/navigator.js
+++ app/src/js/shared/navigator.js
@@ -1,29 +1,67 @@
 var React = require("react");
-var DropDownMenu = require('material-ui/lib/drop-down-menu');
-var menuItems = [
-	{ payload: '1', text: '分类' },
-	{ payload: '2', text: '类型1' },
-	{ payload: '3', text: '类型2' },
-	{ payload: '4', text: '类型3' },
-	{ payload: '5', text: '类型4' }
-];
+var Nav = require('react-bootstrap').Nav;
+var NavDropdown = require('react-bootstrap').NavDropdown;
+var MenuItem = require('react-bootstrap').MenuItem;
+var NavItem = require('react-bootstrap').NavItem;
+
 var Navigator = React.createClass({
 	render: function() {
 		return (
-			<div className = "navigator">
+			<div className = "navigator navbar navbar-default">
 				<div className = "navigator-container">
-					<div className = "navigator-buttons">
-						<div className = "navigator-buttons-button-firstChild">首页</div>
-						<div className = "navigator-buttons-button"><DropDownMenu menuItems={menuItems} /></div>
-						<div className = "navigator-buttons-button"><DropDownMenu menuItems={menuItems} /></div>
-						<div className = "navigator-buttons-button"><DropDownMenu menuItems={menuItems} /></div>
-						<div className = "navigator-buttons-button"><DropDownMenu menuItems={menuItems} /></div>
+					<div className= "col-sm-1"></div>
+					<div className= "col-sm-10 navigator-container-content">
+						<div className= "col-sm-2">
+							<div className="navigator-logo left">Canada Beauty Plus LOGO</div>
+						</div>
+						<div className= "col-sm-8">
+							<div className = "navigator-buttons">
+								<Nav justified bsStyle="tabs" activeKey={1} onSelect={this.handleSelect}>
+									<NavItem eventKey={1} href="/*">首页</NavItem>
+									<NavItem eventKey={2} title="Item">关于我们</NavItem>
+									<NavDropdown eventKey={3} title="分类1" id="nav-dropdown">
+										<MenuItem eventKey="3.1">Action</MenuItem>
+										<MenuItem eventKey="3.2">Another action</MenuItem>
+										<MenuItem eventKey="3.3">Something else here</MenuItem>
+										<MenuItem divider />
+										<NavDropdown eventKey={3.4} title="Link" id="nav-dropright" noCaret={true} className="nav-dropDown-subMenu">
+											<MenuItem eventKey="3.4.1">Action</MenuItem>
+											<MenuItem eventKey="3.4.2">Another action</MenuItem>
+											<MenuItem eventKey="3.4.3">Something else here</MenuItem>
+											<MenuItem divider />
+											<MenuItem eventKey="3.4.4">Separated link</MenuItem>
+										</NavDropdown>
+									</NavDropdown>
+									<NavDropdown eventKey={4} title="分类2" id="nav-dropdown">
+										<MenuItem eventKey="4.1">Action</MenuItem>
+										<MenuItem eventKey="4.2">Another action</MenuItem>
+										<MenuItem eventKey="4.3">Something else here</MenuItem>
+										<MenuItem divider />
+										<MenuItem eventKey="4.4">Separated link</MenuItem>
+									</NavDropdown>
+									<NavDropdown eventKey={5} title="分类3" id="nav-dropdown">
+										<MenuItem eventKey="5.1">Action</MenuItem>
+										<MenuItem eventKey="5.2">Another action</MenuItem>
+										<MenuItem eventKey="5.3">Something else here</MenuItem>
+										<MenuItem divider />
+										<MenuItem eventKey="5.4">Separated link</MenuItem>
+									</NavDropdown>
+								</Nav>
+							</div>
+						</div>
+						<div className= "col-sm-2">
+						</div>
 					</div>
+					<div className= "col-sm-1"></div>
 				</div>
 			</div>
 		);
 	},
 
+	handleSelect: function(eventKey) {
+		event.preventDefault();
+		console.log("selected", eventKey);
+	}
 });
 
 module.exports = Navigator;
